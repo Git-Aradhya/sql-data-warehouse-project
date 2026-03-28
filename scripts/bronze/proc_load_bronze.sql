d6106ef1afa8c1487fb22204ae6f61edc506223f
Aradhya <aradhyamalhotra3@gmail.com>
2026-03-28 11:55:11 +0530

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,72 @@
+/*
+Stored Procedure: Load Bronze Layer
+
+Purpose: 
+It loads data in the bronze schema from external csv files.
+-It truncates the bronze tables before loading data.
+-Uses 'Bulk Insert' command to load data from csv files to bronze tables.
+*/
+
+create or alter procedure bronze.load_bronze as
+begin
+	begin try
+		truncate table bronze.crm_cust_info;
+		bulk insert bronze.crm_cust_info
+		from 'C:\Users\Aradhya\Desktop\New folder\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		with (
+		firstrow=2,
+		fieldterminator=',',
+		tablock
+		); 
+
+		truncate table bronze.crm_prd_info;
+		bulk insert bronze.crm_prd_info
+		from 'C:\Users\Aradhya\Desktop\New folder\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		with (
+		firstrow=2,
+		fieldterminator=',',
+		tablock
+		); 
+
+		truncate table bronze.crm_sales_details;
+		bulk insert bronze.crm_sales_details
+		from 'C:\Users\Aradhya\Desktop\New folder\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		with (
+		firstrow=2,
+		fieldterminator=',',
+		tablock
+		); 
+
+		truncate table bronze.erp_cust_az12;
+		bulk insert bronze.erp_cust_az12
+		from 'C:\Users\Aradhya\Desktop\New folder\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		with (
+		firstrow=2,
+		fieldterminator=',',
+		tablock
+		); 
+
+		truncate table bronze.erp_loc_a101;
+		bulk insert bronze.erp_loc_a101
+		from 'C:\Users\Aradhya\Desktop\New folder\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		with (
+		firstrow=2,
+		fieldterminator=',',
+		tablock
+		); 
+
+		truncate table bronze.erp_px_cat_g1v2;
+		bulk insert bronze.erp_px_cat_g1v2
+		from 'C:\Users\Aradhya\Desktop\New folder\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		with (
+		firstrow=2,
+		fieldterminator=',',
+		tablock
+		); 
+	end try
+	begin catch
+		print 'error occured'
+	end catch
+end
+
+exec bronze.load_bronze
